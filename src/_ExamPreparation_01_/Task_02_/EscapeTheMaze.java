b50edf54da452cdb1bd5b95c64789e04a2c9d7a4
VGoychev <vladimirgoichev@gmail.com>
2024-10-11 21:22:50 +0300

    Initial commit

--- src/_ExamPreparation_01_/Task_02_/EscapeTheMaze.java
+++ src/_ExamPreparation_01_/Task_02_/EscapeTheMaze.java
@@ -15,13 +15,15 @@ public class EscapeTheMaze {
 
         maze = new String[n][n];
 
-
+        // Populating the matrix
         for (int rows = 0; rows < n; rows++) {
             String input = scanner.nextLine();
             for (int cols = 0; cols < n; cols++) {
                 char c = input.charAt(cols);
                 maze[rows][cols] = String.valueOf(c);
                 if (String.valueOf(c).equals("P")){
+
+                    // Getting starting position coordinates
                     startRow = rows;
                     startCol = cols;
                     maze[startRow][startCol] = "-";
@@ -34,10 +36,13 @@ public class EscapeTheMaze {
 
             move(command);
 
+            // Checking if exit is reached
             if (maze[startRow][startCol].equals("X")){
                 maze[startRow][startCol] = "P";
                 System.out.println("Player escaped the maze. Danger passed!");
                 System.out.println("Player's health: " + health + " units");
+
+                //Printing the maze
                 for (int rows = 0; rows < n; rows++) {
                     for (int cols = 0; cols < n; cols++) {
                         System.out.print(maze[rows][cols]);
@@ -47,11 +52,14 @@ public class EscapeTheMaze {
                 return;
             }
 
+            // Checking if Player is dead
             if (health <= 0){
                 maze[startRow][startCol] = "P";
                 health = 0;
                 System.out.println("Player is dead. Maze over!");
                 System.out.println("Player's health: " + health + " units");
+
+                // Printing the maze
                 for (int rows = 0; rows < n; rows++) {
                     for (int cols = 0; cols < n; cols++) {
                         System.out.print(maze[rows][cols]);
@@ -82,6 +90,8 @@ public class EscapeTheMaze {
                 newCol = startCol - 1;
                 break;
         }
+
+        //If it is in bounds let the startRow and startCol take the values out of the newRow and newCol
         if (isInBounds(newRow,newCol)){
             startRow = newRow;
             startCol = newCol;
@@ -102,6 +112,8 @@ public class EscapeTheMaze {
     }
 
     private static boolean isInBounds(int newRow, int newCol){
+
+        // Checking if in bounds
         return newRow >= 0 && newRow < n && newCol >= 0 && newCol < n;
     }
 }
